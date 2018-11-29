cef582196109cdfe454da2f9eb32ba38635ffff9
Pasi <pasi.puumalainen@hotmail.com>
2018-11-29 11:24:04 +0100

    minor change in the coin name

--- project_blockchain/tokensmartcontract.sol
+++ project_blockchain/tokensmartcontract.sol
@@ -1,46 +1,46 @@
 pragma solidity ^0.4.11;
 
-contract jstcoin_ico {
-    //coins available for sale
-    uint public max_jstcoins = 10000000;
+contract onycoin_ico {
+    //coins available for sale, onycoin is our coin/token which is used
+    uint public max_onycoins = 100000000;
     
-    //exchange rate eur to jstc
-    uint public eur_to_jstcoins = 1000;
+    //exchange rate eur to onycoin
+    uint public eur_to_onycoins = 1000;
     
     //bought
-    uint public total_jstcoins_bought = 0;
+    uint public total_onycoins_bought = 0;
     
-    //mapping from the investor address to its equity in jstcoins and eur
-    mapping(address => uint) equity_jstcoins;
+    //mapping from the investor address to its equity in onycoins and eur
+    mapping(address => uint) equity_onycoins;
     mapping(address => uint) equity_eur;
     
-    //checking if an investor can buy jstcoins
-    modifier can_buy_jstcoins(uint eur_invested){
-        require (eur_invested * eur_to_jstcoins <= max_jstcoins);
+    //checking if an investor can buy onycoins
+    modifier can_buy_onycoins(uint eur_invested){
+        require (eur_invested * eur_to_onycoins <= max_onycoins);
         _;
     }    
     
-    function equity_in_jstcoins(address investor) external constant returns (uint) {
-        return equity_jstcoins[investor];
+    function equity_in_onycoins(address investor) external constant returns (uint) {
+        return equity_onycoins[investor];
     }
     
     function eqity_in_eur(address investor) external constant returns (uint){
         return equity_eur[investor];
     }
     
-    function buy_jstcoins(address investor, uint eur_invested) external
-    can_buy_jstcoins(eur_invested) {
-        uint jstcoins_bought = eur_invested * eur_to_jstcoins;
-        equity_jstcoins[investor] += jstcoins_bought;
-        equity_eur[investor] = equity_jstcoins[investor] / 1000;
-        total_jstcoins_bought += jstcoins_bought;
+    function buy_onycoins(address investor, uint eur_invested) external
+    can_buy_onycoins(eur_invested) {
+        uint onycoins_bought = eur_invested * eur_to_onycoins;
+        equity_onycoins[investor] += onycoins_bought;
+        equity_eur[investor] = equity_onycoins[investor] / 1000;
+        total_onycoins_bought += onycoins_bought;
         
     }
     
-    function sell_jstcoins(address investor, uint jstcoins_sold) external {
-        equity_jstcoins[investor] -= jstcoins_sold;
-        equity_eur[investor] = equity_jstcoins[investor] / 1000;
-        total_jstcoins_bought -= jstcoins_sold;
+    function sell_jstcoins(address investor, uint onycoins_sold) external {
+        equity_onycoins[investor] -= onycoins_sold;
+        equity_eur[investor] = equity_onycoins[investor] / 1000;
+        total_onycoins_bought -= onycoins_sold;
     }
     
 }
