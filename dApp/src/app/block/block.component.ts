1afbc2ebf369fce84e9f6a76ff6100896a700afc
TBRRayz <bryanderonde@hotmail.com>
2018-12-10 13:42:03 +0100

    make the interface for the block card page

--- dApp/src/app/block/block.component.ts
+++ dApp/src/app/block/block.component.ts
@@ -1,7 +1,12 @@
 /**
  * Created by bryan on 4-12-2018.
  */
-import { Component} from '@angular/core';
+import { Component, HostListener, OnInit} from '@angular/core';
+
+export interface Option {
+  value: string;
+  viewValue: string;
+}
 
 
 @Component({
@@ -11,4 +16,40 @@ import { Component} from '@angular/core';
   styleUrls: ['block.component.css'],
 })
 
-export class BlockComponent{}
+export class BlockComponent{
+  visible: boolean = true;
+  checked = false;
+  breakpoint: number = 520;
+  selectedOption : string;
+
+
+  options: Option[] = [
+    {value: 'lost', viewValue: 'Lost'},
+    {value: 'stolen', viewValue: 'Stolen'},
+    {value: 'dontused', viewValue: 'Dont Used'}
+  ];
+
+  ngOnInit() {
+    const w = window.innerWidth;
+    if (w >= this.breakpoint) {
+      this.visible = true;
+    } else {
+      // whenever the window is less than 520, hide this component.
+      this.visible = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    const w = event.target.innerWidth;
+    if (w >= this.breakpoint) {
+      this.visible = true;
+    } else {
+      // whenever the window is less than 520, hide this component.
+      this.visible = false;
+    }
+  }
+
+  goToRegister(){}
+
+}
