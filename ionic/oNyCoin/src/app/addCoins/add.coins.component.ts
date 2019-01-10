afc4a1cbe91359179e2ab878f945efb40d3e1ecf
Cas <casvantwout@gmail.com>
2019-01-10 15:10:58 +0100

    refund works

--- ionic/oNyCoin/src/app/addCoins/add.coins.component.ts
+++ ionic/oNyCoin/src/app/addCoins/add.coins.component.ts
@@ -1,43 +1,54 @@
 /**
  * Created by bryan on 4-12-2018.
  */
-import { Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Web3Service} from "../util/web3.service";
 
 
 @Component({
-  moduleId: module.id,
-  selector: 'rg-add-coins',
-  templateUrl: 'add.coins.component.html',
-  styleUrls: ['add.coins.component.css'],
+    moduleId: module.id,
+    selector: 'rg-add-coins',
+    templateUrl: 'add.coins.component.html',
+    styleUrls: ['add.coins.component.css'],
 })
 
-export class AddCoinsComponent{
+export class AddCoinsComponent {
 
-  visible: boolean = true;
-  breakpoint: number = 520;
+    visible: boolean = true;
+    breakpoint: number = 520;
 
-  ngOnInit() {
-    const w = window.innerWidth;
-    if (w >= this.breakpoint) {
-      this.visible = true;
-    } else {
-      // whenever the window is less than 520, hide this component.
-      this.visible = false;
+    constructor(private web3Service: Web3Service) {
+
     }
-  }
 
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    const w = event.target.innerWidth;
-    if (w >= this.breakpoint) {
-      this.visible = true;
-    } else {
-      // whenever the window is less than 520, hide this component.
-      this.visible = false;
+    ngOnInit() {
+        const w = window.innerWidth;
+        if (w >= this.breakpoint) {
+            this.visible = true;
+        } else {
+            // whenever the window is less than 520, hide this component.
+            this.visible = false;
+        }
     }
-  }
 
-  goToRegister(){}
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        const w = event.target.innerWidth;
+        if (w >= this.breakpoint) {
+            this.visible = true;
+        } else {
+            // whenever the window is less than 520, hide this component.
+            this.visible = false;
+        }
+    }
+
+    goToRegister() {
+    }
+
+    addCoins(amount) {
+        console.log(amount);
+        this.web3Service.buyTokens(amount);
+    }
 
 }
